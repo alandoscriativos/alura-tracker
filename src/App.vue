9be098642ac62064ffa0a8c8c9b673375cdfa883
Alan <allanrds26@gmail.com>
2023-03-06 16:35:17 -0300

    Formulario Pronto

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         </div>
 
         <div class="column is-three-quarter">
-        
+          <Formulario/>
         </div>
     </main>
 </template>
@@ -13,12 +13,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
 
 
 export default defineComponent({
   name: 'App',
   components: {
-    BarraLateral
+    BarraLateral,
+    Formulario
   }
 
 });
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,17 +1,40 @@
 <template>
     <div class='box'>
         <div class='columns'>
-            <div class='column is-8'><div>
+            <div class='column is-8' role='form' aria-label='Formulário de criação de uma nova tarega'>
+                <input type='text'
+                class='input'
+                placeholder='Qual tarefa você deseja iniciar?'>
+            </div>
+            <div class='column'>
+                <div class='is-flex is-align-items-center is-justify-content-space-between'>
+                    <section>
+                        <strong>00:00:00</strong>
+                    </section>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang='ts'>
-import {defineComponent} from 'vue';
-export default defineComponent({ 
-    Formulario
-})
-</script>
 
-<style>
-</style>
+import { defineComponent } from 'vue';
+
+export default defineComponent({ 
+    name: 'FormuLario'
+})
+
+</script>
